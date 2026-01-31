8ead3964c7cfe0b4b5c5a1c92aa4cb71de56ce50
Erd <undefined>
2026-01-31 03:39:44 +0000

    fix: Use node: prefix for imports in publishers

--- packages/git-flow/src/publishing/publishers.ts
+++ packages/git-flow/src/publishing/publishers.ts
@@ -1,5 +1,5 @@
-import { writeFile } from 'fs/promises';
-import { dirname, join } from 'path';
+import { writeFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
 import { $ } from 'zx';
 import type { NpmPublishOptions, NugetPublishOptions, DockerPublishOptions } from './types.js';
 
